3c0ea87625a20080ab7b470b4e1fcd2411ee491b
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-11-12 17:03:14 +0900

    feat: remove unused annotations

--- src/main/java/com/kintone/client/model/app/AppStatisticsStatus.java
+++ src/main/java/com/kintone/client/model/app/AppStatisticsStatus.java
@@ -1,18 +1,13 @@
 package com.kintone.client.model.app;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /** The status of App settings. */
 public enum AppStatisticsStatus {
     /** The app settings are changed but not activated yet. */
-    @JsonProperty("CHANGED")
     CHANGED,
 
     /** The app is not activated yet. */
-    @JsonProperty("NOT_ACTIVATED")
     NOT_ACTIVATED,
 
     /** The app is activated. */
-    @JsonProperty("ACTIVATED")
     ACTIVATED
 }
