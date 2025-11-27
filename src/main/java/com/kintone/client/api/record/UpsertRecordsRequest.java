5cb96079cef73e1a5813b6be26a0e5b3281a18b7
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-11-27 15:36:19 +0900

    refactor: format

--- src/main/java/com/kintone/client/api/record/UpsertRecordsRequest.java
+++ src/main/java/com/kintone/client/api/record/UpsertRecordsRequest.java
@@ -15,7 +15,7 @@ public class UpsertRecordsRequest implements KintoneRequest {
     /** A list of objects that include id/updateKey, revision and record objects (required). */
     private List<RecordForUpdate> records;
 
-   /**
+    /**
      * If set to true, the UPSERT mode will be enabled. In UPSERT mode, if the specified record
      * exists, it will be updated. If it does not exist, a new record will be created (optional).
      */
